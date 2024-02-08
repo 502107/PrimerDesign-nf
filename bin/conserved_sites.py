e5f0e8c6fe5be1164c36fd7314f2f4af10781ca7
502107 <107959213+502107@users.noreply.github.com>
2024-02-08 16:08:38 +0000

    Update conserved_sites.py

--- bin/conserved_sites.py
+++ bin/conserved_sites.py
@@ -28,34 +28,7 @@ def read_blast(blast_output, pident_thresh=94, sseq_len_thresh=1000):
                     results[qseqid] = {'sseqid': sseqid, 'pident': pident, 'bitscore': bitscore, 'score': score, 'sstart': sstart, 'send': send, 'sseq': sseq}
     return results
 
-# First try at aligning, but we need it to handle MSA better
-# def align_seqs(sequences):
-#         aligner = Align.PairwiseAligner()
-#         aligner.substitution_matrix = substitution_matrices.load("BLOSUM62")
-#         aligned_seqs = []
-#         # Aligning sequences to the first sequence
-#         sequences_edit = [sub.replace("-", "") for sub in sequences]
-#         ref_seq = sequences_edit[0]
-#         for seq in sequences_edit[1:]:
-#             alignments = aligner.align(ref_seq, seq)
-#             aligned_seq = str(alignments[0].aligned[1])
-#             aligned_seqs.append(aligned_seq)
-#         aligned_seqs.insert(0, ref_seq)
-#         return aligned_seqs
-
-# Second try, should've worked but getting clustalw2 to do what i want is a pain
-# def align_seqs(sequences):
-#     with open("tmp.fna", "w") as f:
-#         for i, seq in enumerate(sequences):
-#             f.write(f">seq{i}\n{seq}\n")
-#     # clustalomega_cline = ClustalwCommandline("clustalw2",infile="tmp.fna", outfile="tmp-aln.fna")
-#     subprocess.run(["clustalw2", "-infile=tmp.fna", "-outfile=tmp-aln.fna"], check=True)
-#     # clustalomega_cline()
-#     align = AlignIO.read("tmp-aln.fna", "fasta")
-#     return [str(rec.seq) for rec in align]
-
-# Third try, basically same as second but calling clustalw2 directly
-# Also, the output is an interleaved fasta file by default, so defining the output as fasta to have a sequential output
+# The output is an interleaved fasta file by default, so defining the output as fasta to have a sequential output
 def align_seqs(sequences):
     with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_input:
         for i, seq in enumerate(sequences):
@@ -83,35 +56,21 @@ def find_conserved_seq(sequences):
             conserved_seq += "-"
             
     return conserved_seq
-        
-    # alignment_file_path = align_seqs(sequences)
-    # with open(alignment_file_path, 'r') as alignment_file:
-    #     alignment = AlignIO.read(alignment_file, "fasta")
-    # summary_align = AlignInfo.SummaryInfo(alignment)
-    # consensus = summary_align.dumb_consensus(ambiguous='-')
-    # os.remove(alignment_file_path)
-    # return str(consensus)
 
 
 def find_conserved_sites(blast_results):
     conserved_sites = {}
-    # The dictionary looks like that: qseqid: {sseqid: sseq},
-    # so need to iterate over it twice, and use a list to get each of the sub-items
+    # The dictionary looks like that: qseqid: {sseqid: sseq}
     for qseqid, hit_lists in blast_results.items():
         sequences = [hit['sseq'] for hit in hit_lists]
         
         # Now that we have the sequences in a dictionary, we need to find the conserved sites between them, but first need to convert the sequences to a list
-        # for hit in hit_lists: 
-        #     if 'sseq' in hit:
-        #         sequences.append(hit['sseq'])
-        
         if sequences:
             conserved_seq = find_conserved_seq(sequences)
             if conserved_seq:
                 conserved_sites[qseqid] = conserved_seq
     return conserved_sites
 
-# OK, let's pull it all together and see what happens
 def main():
     if len(sys.argv) < 3:
         raise ValueError("Usage: python conserved_sites.py <rust_type> <blast_process_output_dir> <output_file>")
