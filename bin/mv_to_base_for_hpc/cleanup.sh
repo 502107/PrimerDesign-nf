278c67337ff3c1252ac027f5a79f8b92c95cfadb
502107 <107959213+502107@users.noreply.github.com>
2024-02-16 09:27:00 +0000

    Update cleanup.sh

--- bin/mv_to_base_for_hpc/cleanup.sh
+++ bin/mv_to_base_for_hpc/cleanup.sh
@@ -11,13 +11,7 @@ done
 pgt_primers='data/primers/pgt_primers.csv'
 pst_primers='data/primers/pst_primers.csv'
 
-head -n 1 -q data/primers/pgt/* | uniq > $pgt_primers
-tail -n 1 -q data/primers/pgt/* >> $pgt_primers
-sort -k 2 -n -o $pgt_primers $pgt_primers
-
-head -n 1 -q data/primers/pst/* | uniq > $pst_primers
-tail -n 1 -q data/primers/pst/* >> $pst_primers
-sort -k 2 -n -o $pst_primers $pst_primers
+(head -n 1 -q data/primers/pgt/* | uniq && tail -n 1 -q data/primers/pgt/* | sort -k 2 -rn) > $pgt_primers
+(head -n 1 -q data/primers/pst/* | uniq && tail -n 1 -q data/primers/pst/* | sort -k 2 -rn) > $pst_primers
 
 rm -rf data/primers/pgt data/primers/pst
-
